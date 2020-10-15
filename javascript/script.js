7f8d4fdc17a65ec6009ac6fe870195f1c43a2b5f
alvianharyanto <alvian.haryanto@gmail.com>
2020-10-15 14:37:50 +0700

    desktop

---
+++ javascript/script.js
@@ -0,0 +1,21 @@
+var question = document.getElementsByClassName("question");
+var i, x;
+for (i = 0; i < question.length; i++) {
+    // add click
+    question[i].addEventListener("click", function () {
+        // function hitung jumlah question & hilangkan aktif
+        for (var x = 0; x < question.length; x++) {
+            question[x].classList.remove("active");
+            question[x].nextElementSibling.classList.remove("show--answer");
+        }
+        // aktifkan jika sesuai kondisi
+        this.classList.toggle("active");
+        var answer = this.nextElementSibling;
+        if (answer.className == "answer show--answer") {
+            answer.classList.remove("show--answer");
+        } else {
+            answer.classList.toggle("show--answer");
+        }
+    });
+}
+question[1].click(); // aktif pertama load
