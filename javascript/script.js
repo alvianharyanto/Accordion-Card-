f6c1bdf00adb122c7c3699ebf87a409d4b27f869
alvianharyanto <alvian.haryanto@gmail.com>
2021-09-29 23:47:42 +0700

    update

--- javascript/script.js
+++ javascript/script.js
@@ -1,17 +1,17 @@
 // accordion show - hide effect
-var question = document.getElementsByClassName("question");
-var i, x;
+const question = document.getElementsByClassName("question");
+let i;
 for (i = 0; i < question.length; i++) {
     // add click
     question[i].addEventListener("click", function () {
         // function hitung jumlah question & hilangkan aktif
-        for (var x = 0; x < question.length; x++) {
-            question[x].classList.remove("active");
-            question[x].nextElementSibling.classList.remove("show--answer");
+        for (i = 0; i < question.length; i++) {
+            question[i].classList.remove("active");
+            question[i].nextElementSibling.classList.remove("show--answer");
         }
         // aktifkan jika sesuai kondisi
         this.classList.toggle("active");
-        var answer = this.nextElementSibling;
+        const answer = this.nextElementSibling;
         if (answer.className == "answer show--answer") {
             answer.classList.remove("show--answer");
         } else {
